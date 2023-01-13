f0e285a19d4959b945d67720baca2b2dd332d5e1
Chris <106836962+Cujion@users.noreply.github.com>
2023-01-13 15:07:00 -0500

    initial commit

---
+++ models/Traveller.js
@@ -0,0 +1,32 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Traveller extends Model {}
+
+Traveller.init({    
+    id: {
+    type: DataTypes.INTEGER,
+    allowNullL: false,
+    primaryKey: true,
+    autoIncrement: true,
+},
+name: {
+    type: DataTypes.STRING,
+    allownull: false,
+},
+email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    validate: {
+        isEmail: true,
+    }
+}
+},{
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'traveller',
+});
+
+module.exports = Traveller
